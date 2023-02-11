d82fb5991bb053346208690681ede547021b9733
OnMyMachineItWorks <124904013+OnMyMachineItWorks@users.noreply.github.com>
2023-02-11 18:13:01 +0530

    AddedCodeFirstTableEntities

--- MoviesTime.DataAccess/Database/ApplicationDbContext.cs
+++ MoviesTime.DataAccess/Database/ApplicationDbContext.cs
@@ -11,13 +11,17 @@ namespace MoviesTime.DataAccess.Database
             
         }
 
-        public DbSet<TestModel> TestTable { get; set; }
         public DbSet<Users> Users { get; set; }
-        public DbSet<Theater> Theater { get; set; }
+        public DbSet<Theaters> Theaters { get; set; }
         public DbSet<Genres> Genres { get; set; }
         public DbSet<Languages> Languages { get; set; }
         public DbSet<Movies> Movies { get; set; }
         public DbSet<MovieLanguageMapping> MovieLanguageMapping { get; set; }
         public DbSet<MovieGenreMapping> MovieGenreMapping { get; set; }
+        public DbSet<MovieReview> MovieReview { get; set; }
+        public DbSet<TheaterReview> TheaterReview { get; set; }
+        public DbSet<TheaterScreen> TheaterScreens { get; set; }
+        public DbSet<MovieShows> MovieShows { get; set; }
+        public DbSet<Tickets> Tickets { get; set; }
     }
 }
